2457c7df6d5a9853a3ad4bb3996ced220fb08560
prayaas on SP6 <prayaas@outlook.com>
2019-12-25 00:28:13 -0500

    Added comments to explain changes

--- NotepadRs4/NotepadRs4/Views/MainPage.xaml.cs
+++ NotepadRs4/NotepadRs4/Views/MainPage.xaml.cs
@@ -263,17 +263,15 @@ namespace NotepadRs4.Views
 
         private void txtContent_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            //Getting current position
-            //ViewModel.Col = txtContent.SelectionStart + 1;
-
             //Getting current line
             string sub = txtContent.Text.Substring(0, txtContent.SelectionStart);
             ViewModel.Line = sub.Count(i => i == '\r') + 1;
 
-            int retIndex = sub.LastIndexOf('\r');
-            retIndex = (retIndex < 0) ? 0 : retIndex;
-            string newsub = sub.Substring(retIndex);
-            ViewModel.Col = newsub.Count();
+            //Getting current pos
+            int retIndex = sub.LastIndexOf('\r'); //we look for a return in the text
+            retIndex = (retIndex < 0) ? 0 : retIndex; //if no return, start counting from the beginning (for line 1)
+            string newsub = sub.Substring(retIndex); //substring from this point on to the end
+            ViewModel.Col = newsub.Count(); //the value we need is simply the count of this new substring
         }
 
         private void svContent_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
